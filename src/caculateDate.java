e88329ece346ae06eb6114dc619d1400d9b434f0
Siegelth <34745350+Siegelth@users.noreply.github.com>
2022-05-23 10:14:38 +0800

    update

--- src/caculateDate.java
+++ src/caculateDate.java
@@ -5,19 +5,20 @@ import java.util.Scanner;
 public class caculateDate {
     caculateDate(){};
     public static void main(String[] args) throws Exception {
-
+        int[] group = new int[3];
         System.out.println("start");
         Scanner input = new Scanner(System.in);
-            int Year = input.nextInt();
-            int Month = input.nextInt();
-            int Day = input.nextInt();
-        LocalDate startDate = LocalDate.of(Year, Month, Day);
+        for(int i = 0 ; i <= 2 ; i++){
+            group[i] = input.nextInt();
+        }
+        LocalDate startDate = LocalDate.of( group[0], group[1] , group[2]);
 
         System.out.println("end");
-            Year = input.nextInt();
-            Month = input.nextInt();
-            Day = input.nextInt();
-        LocalDate endDate = LocalDate.of(Year, Month, Day);
+        for(int i = 0 ; i <= 2 ; i++){
+            group[i] = input.nextInt();
+        }
+        LocalDate endDate = LocalDate.of( group[0], group[1] , group[2]);
+
         input.close();
 
         Long theDays = startDate.until(endDate,ChronoUnit.DAYS);
